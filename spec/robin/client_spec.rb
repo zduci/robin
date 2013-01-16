0b2d2cbcb1f6df0a3a3a2ab5f50e501d933cc93b
Radu Busuioc <ra.busuioc@gmail.com>
2013-01-16 21:00:39 +0000

    Clearer tests

--- spec/robin/client_spec.rb
+++ spec/robin/client_spec.rb
@@ -7,9 +7,8 @@ describe Robin::Client do
     VCR.use_cassette('home_timeline') do
       timeline = Robin::Client.home_timeline
       timeline.size.should == 20
-      timeline[0].full_text.should == 'RT @lancewalton: Performance Related Pay for school teachers is lunacy. Pay teachers more and remove almost all performance measures.'
-        timeline[1].full_text.should == 'RT @BCS_Agile: Honoured that @kjscotland &amp; @PatrickSteyaert will be speakers at the @BCS London Lean #Kanban Day - 16 Mar #LLKD13 http://t.co/DbowsYmZ'
-        timeline[19].full_text.should == 'Hey @sramsay, I wonder what you\'d think of http://t.co/455yw8DP in light of \'algorithmic criticism\'.'
+      timeline[0].full_text.should == "RT @lancewalton: Performance Related Pay for school teachers is lunacy. Pay teachers more and remove almost all performance measures."
+        timeline[19].full_text.should == "Hey @sramsay, I wonder what you'd think of http://t.co/455yw8DP in light of 'algorithmic criticism'."
     end
   end
 end
