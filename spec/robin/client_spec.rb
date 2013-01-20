91c692249573d657f7692ab659514ea5737bcf03
Radu Busuioc <ra.busuioc@gmail.com>
2013-01-20 13:04:17 +0000

    Group specs into contexts

--- spec/robin/client_spec.rb
+++ spec/robin/client_spec.rb
@@ -3,44 +3,52 @@ require 'vcr'
 require 'vcr_helper'
 
 describe Robin::Client do
-  it "retrieves the user's home timeline" do
-    VCR.use_cassette('home_timeline') do
-      timeline = Robin::Client.home_timeline
-      timeline.size.should == 20
-      timeline[0].full_text.should == "RT @lancewalton: Performance Related Pay for school teachers is lunacy. Pay teachers more and remove almost all performance measures."
-      timeline[19].full_text.should == "Hey @sramsay, I wonder what you'd think of http://t.co/455yw8DP in light of 'algorithmic criticism'."
+  context 'home timeline' do
+    it "retrieves the user's home timeline" do
+      VCR.use_cassette('home_timeline') do
+        timeline = Robin::Client.home_timeline
+        timeline.size.should == 20
+        timeline[0].full_text.should == "RT @lancewalton: Performance Related Pay for school teachers is lunacy. Pay teachers more and remove almost all performance measures."
+        timeline[19].full_text.should == "Hey @sramsay, I wonder what you'd think of http://t.co/455yw8DP in light of 'algorithmic criticism'."
+      end
     end
   end
 
-  it "retrieves the user's own timeline" do
-    VCR.use_cassette('user_timeline') do
-      timeline = Robin::Client.user_timeline
-      timeline.size.should == 20
-      timeline[0].full_text.should == "please make the gaga stop"
-      timeline[19].full_text.should == "set -o vi"
+  context 'user timeline' do
+    it "retrieves the user's own timeline" do
+      VCR.use_cassette('user_timeline') do
+        timeline = Robin::Client.user_timeline
+        timeline.size.should == 20
+        timeline[0].full_text.should == "please make the gaga stop"
+        timeline[19].full_text.should == "set -o vi"
+      end
     end
   end
 
-  it "tweets a new tweet" do
-    VCR.use_cassette('new_tweet') do
-      Robin::Client.tweet('test')
-      Robin::Client.user_timeline.first.full_text.should == 'test'
+  context 'new tweet' do
+    it "tweets a new tweet" do
+      VCR.use_cassette('new_tweet') do
+        Robin::Client.tweet('test')
+        Robin::Client.user_timeline.first.full_text.should == 'test'
+      end
     end
   end
 
-  it "retrieves the user's followers" do
-    VCR.use_cassette('user_followers') do
-      followers = Robin::Client.followers
-      followers[0].name.should == "Sang Jakob"
-      followers[14].name.should == "Duncan Grant"
+  context 'followers' do
+    it "retrieves the user's followers" do
+      VCR.use_cassette('user_followers') do
+        followers = Robin::Client.followers
+        followers[0].name.should == "Sang Jakob"
+        followers[14].name.should == "Duncan Grant"
+      end
     end
-  end
 
-  it "retrieves another user's followers" do
-    VCR.use_cassette('another_user_followers') do
-      followers = Robin::Client.followers("carvalho82")
-      followers[0].name.should == "Gill Gillies"
-      followers[1].name.should == "Ros McDonagh"
+    it "retrieves another user's followers" do
+      VCR.use_cassette('another_user_followers') do
+        followers = Robin::Client.followers("carvalho82")
+        followers[0].name.should == "Gill Gillies"
+        followers[1].name.should == "Ros McDonagh"
+      end
     end
   end
 end
--- spec/robin_spec.rb
+++ spec/robin_spec.rb
@@ -3,55 +3,63 @@ require 'vcr'
 require 'vcr_helper'
 
 describe 'robin' do
-  it "prints the user's home timeline" do
-    VCR.use_cassette('home_timeline') do
-      Runner.run
-      expect(Runner.output).to start_with "RT @lancewalton: Performance Related Pay for school teachers is lunacy. Pay teachers more and remove almost all performance measures."
-      expect(Runner.output).to end_with "Hey @sramsay, I wonder what you'd think of http://t.co/455yw8DP in light of 'algorithmic criticism'.\n"
+  context 'home timeline' do
+    it "prints the user's home timeline" do
+      VCR.use_cassette('home_timeline') do
+        Runner.run
+        expect(Runner.output).to start_with "RT @lancewalton: Performance Related Pay for school teachers is lunacy. Pay teachers more and remove almost all performance measures."
+        expect(Runner.output).to end_with "Hey @sramsay, I wonder what you'd think of http://t.co/455yw8DP in light of 'algorithmic criticism'.\n"
+      end
     end
   end
 
-  it "prints the user's latest tweets" do
-    VCR.use_cassette('user_timeline') do
-      Runner.run(['-i'])
-      expect(Runner.output).to start_with "please make the gaga stop"
-      expect(Runner.output).to end_with "set -o vi\n"
+  context 'latest tweets' do
+    it "prints the user's latest tweets" do
+      VCR.use_cassette('user_timeline') do
+        Runner.run(['-i'])
+        expect(Runner.output).to start_with "please make the gaga stop"
+        expect(Runner.output).to end_with "set -o vi\n"
+      end
     end
   end
 
-  it "tweets and prints the tweet" do
-    VCR.use_cassette('new_tweet') do
-      Runner.run(['-t', 'test'])
-      expect(Runner.output).to eq "test\n"
+  context 'new tweet' do
+    it "tweets and prints the tweet" do
+      VCR.use_cassette('new_tweet') do
+        Runner.run(['-t', 'test'])
+        expect(Runner.output).to eq "test\n"
 
-      Runner.run(['-i'])
-      expect(Runner.output).to start_with 'test'
+        Runner.run(['-i'])
+        expect(Runner.output).to start_with 'test'
+      end
+    end
+
+    it "tweets without the -t flag" do
+      VCR.use_cassette('new_tweet') do
+        Runner.run(['test'])
+        expect(Runner.output).to eq "test\n"
+
+        Runner.run(['-i'])
+        expect(Runner.output).to start_with 'test'
+      end
     end
   end
 
-  it "tweets without the -t flag" do
-    VCR.use_cassette('new_tweet') do
-      Runner.run(['test'])
-      expect(Runner.output).to eq "test\n"
-
-      Runner.run(['-i'])
-      expect(Runner.output).to start_with 'test'
+  context 'followers' do
+    it "prints the user's followers" do
+      VCR.use_cassette('user_followers') do
+        Runner.run(['-fr'])
+        expect(Runner.output).to start_with "Sang Jakob (Carleyahoj)"
+        expect(Runner.output).to end_with "Syprian (siopenschtein)\n"
+      end
     end
-  end
 
-  it "prints the user's followers" do
-    VCR.use_cassette('user_followers') do
-      Runner.run(['-fr'])
-      expect(Runner.output).to start_with "Sang Jakob (Carleyahoj)"
-      expect(Runner.output).to end_with "Syprian (siopenschtein)\n"
-    end
-  end
-
-  it "prints the followers of another user" do
-    VCR.use_cassette('another_user_followers') do
-      Runner.run(['-fr', 'carvalho82'])
-      expect(Runner.output).to start_with "Gill Gillies"
-      expect(Runner.output).to end_with "Jaime Oliveira (jaimeoliv)\n"
+    it "prints the followers of another user" do
+      VCR.use_cassette('another_user_followers') do
+        Runner.run(['-fr', 'carvalho82'])
+        expect(Runner.output).to start_with "Gill Gillies"
+        expect(Runner.output).to end_with "Jaime Oliveira (jaimeoliv)\n"
+      end
     end
   end
 end
